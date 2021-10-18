9044fb9ac329cb386e41f162442c1393096e2033
orlaneee <90193590+orlaneee@users.noreply.github.com>
2021-10-18 14:58:03 +0200

    Add files via upload

---
+++ TD_turtle.py
@@ -0,0 +1,35 @@
+import turtle
+from turtle import circle, forward, right, left, bye, pendown, penup, pendown, goto, speed
+
+circle(50)
+right(90)
+penup()
+forward(25)
+
+pendown()
+forward(50)
+
+for i in range(1,3):
+    left(90)
+    forward(300)
+    left(90)
+    forward(100)
+
+#Exo1
+
+def dessine(n):
+    if n <= 0:
+        return None
+    else :
+        turtle.forward(n)
+        turtle.right(90)
+    return dessine(n-10)
+
+print(dessine(500))
+
+#Exo2
+
+def Von_Koch(n,l):
+    if n == 0:
+        return turtle.forward(l)
+    else:
