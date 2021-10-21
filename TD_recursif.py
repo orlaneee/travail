bd0e7e220fcf711d7c1fd39836ba9ad36936c7f0
orlaneee <90193590+orlaneee@users.noreply.github.com>
2021-10-21 09:25:40 +0200

    Update TD_recursif.py

--- TD_recursif.py
+++ TD_recursif.py
@@ -32,8 +32,34 @@ def factorielle_recursif(n):
     else:
         return n * (factorielle_recursif(n-1))
 
-    #Exercice 5
-#question 1
+#Exercice5
+
+"""fonction récursive puissance qui :
+prend deux entiers a et n (avec n >= 0)"""
+
+def recursive(a,n):
+    if n == 0:
+        return 1
+    else:
+        
+        return recursive(a,n-1) * a
+    
+#print(recursive(2,3))
+
+def recursivev2(a,n):
+    if n == 0:
+        return 1
+    elif (n % 2 == 0):
+        return a*(recursivev2(a, n /2))
+    else:
+        
+        return a * (a * (recursivev2(a, n-1)/2))
+    
+print(recursivev2(2,3))
+
+# avec la fonction recursive ça sera plus long car ça fera 1-1,1-1... tandis que la fonction recursivev2 divisera par deux dès qu'on est sur un nombre paire
+
+
 def recursive(a,n):
     if n == 0:
         return 1
