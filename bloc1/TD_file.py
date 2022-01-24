3a72abf067be9be399662147ecc520cc2d288c93
orlaneee <90193590+orlaneee@users.noreply.github.com>
2022-01-24 14:46:57 +0100

    Add files via upload

---
+++ bloc1/TD_file.py
@@ -0,0 +1,20 @@
+from File import *
+
+f = File() # Création de l'objet 
+f.enfile(5)
+f.enfile(3)
+f.enfile(7)
+
+#Act 1.2
+#f.defile()
+
+#Act 1.3
+# assert sert a vérifier si la file est vide ou pas
+
+#Act 1.4
+# .pop sert à retirer l'élément demander
+
+
+#exo 5
+def new_file(file):
+    
